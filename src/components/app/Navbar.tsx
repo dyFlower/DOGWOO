2638160f54677ca87a208aff2b00cf1e74fd586f
Doyoung.Kim <dyflower@nate.com>
2023-06-18 03:19:24 +0900

    :lipstick: Design : add sign-in button
    로그인 버튼 부착

--- src/components/app/Navbar.tsx
+++ src/components/app/Navbar.tsx
@@ -2,12 +2,13 @@ import { Link, useLocation } from 'react-router-dom';
 import Pow from '../../assets/icons/pow.png';
 import DefaultProfile from '../../assets/icons/DefaultProfile.png';
 import { Fragment, useEffect, useState } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Disclosure, Menu, Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import useWeather from '../weather/useWeather';
 import { translateWeather } from '../weather/translate';
 
 const Navbar = () => {
+  const user = false;
   const location = useLocation();
   const [currentPage, setCurrentPage] = useState('');
   const navigation = [
@@ -73,37 +74,21 @@ const Navbar = () => {
                 </div>
               </div>
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                <button
-                  type='button'
-                  className='flex items-center rounded-full bg-green p-1 px-3 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                >
-                  <span className='sr-only'>날씨</span>
-                  {isLoading ? 'Loading...' : translateWeather(data?.weather[0].main)}
-                  {isLoading ? (
-                    ''
-                  ) : (
-                    <img
-                      className='h-8 w-8'
-                      src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
-                      alt='날씨'
-                    />
-                  )}
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as='div' className='relative ml-3'>
-                  <div>
-                    <Menu.Button className='flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                      <span className='sr-only'>프로필 정보 열기</span>
+                <Popover>
+                  <Popover.Button className='flex items-center rounded-full bg-green p-1 px-3 text-white hover:bg-hovergreen focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                    <span className='sr-only'>날씨</span>
+                    {isLoading ? 'Loading...' : translateWeather(data?.weather[0].main)}
+                    {isLoading ? (
+                      ''
+                    ) : (
                       <img
-                        className='h-8 w-8 rounded-full'
-                        src={DefaultProfile}
-                        alt='프로필 사진'
+                        className='h-8 w-8'
+                        src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+                        alt='날씨'
                       />
-                    </Menu.Button>
-                  </div>
+                    )}
+                  </Popover.Button>
                   <Transition
-                    as={Fragment}
                     enter='transition ease-out duration-100'
                     enterFrom='transform opacity-0 scale-95'
                     enterTo='transform opacity-100 scale-100'
@@ -111,49 +96,102 @@ const Navbar = () => {
                     leaveFrom='transform opacity-100 scale-100'
                     leaveTo='transform opacity-0 scale-95'
                   >
-                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }: any) => (
-                          <Link
-                            to='/profile/:id'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700',
-                            )}
-                          >
-                            내 프로필
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }: any) => (
-                          <Link
-                            to='/profile/:id/:dog'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700',
-                            )}
-                          >
-                            멍멍이 프로필
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }: any) => (
-                          <Link
-                            to='/'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700',
-                            )}
-                          >
-                            로그아웃
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
+                    <Popover.Panel className='absolute z-10 top-2 right-1'>
+                      <div className='rounded-md bg-white p-5 flex-row'>
+                        <span className='block font-bold text-3xl'>{data?.name}</span>
+                        <span className='block font-semibold text-2xl text-center'>
+                          {data?.main.temp}°
+                        </span>
+                        <div>
+                          <span className='text-gray-400'>체감 : </span>{' '}
+                          <span className=''>{data?.main.feels_like}°</span>
+                        </div>
+                        <div>
+                          <span className='text-gray-400'>습도 : </span>
+                          <span className=''>{data?.main.humidity} %</span>
+                        </div>
+                        <div>
+                          <span className='text-gray-400'>풍속 : </span>
+                          <span className=''>{data?.wind.speed} m/s</span>
+                        </div>
+                      </div>
+                    </Popover.Panel>
                   </Transition>
-                </Menu>
+                </Popover>
+
+                {/* Profile dropdown */}
+                {user ? (
+                  <Menu as='div' className='relative ml-3'>
+                    <div>
+                      <Menu.Button className='flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                        <span className='sr-only'>프로필 정보 열기</span>
+                        <img
+                          className='h-8 w-8 rounded-full'
+                          src={DefaultProfile}
+                          alt='프로필 사진'
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter='transition ease-out duration-100'
+                      enterFrom='transform opacity-0 scale-95'
+                      enterTo='transform opacity-100 scale-100'
+                      leave='transition ease-in duration-75'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'
+                    >
+                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                        <Menu.Item>
+                          {({ active }: any) => (
+                            <Link
+                              to='/profile/:id'
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700',
+                              )}
+                            >
+                              내 프로필
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }: any) => (
+                            <Link
+                              to='/profile/:id/:dog'
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700',
+                              )}
+                            >
+                              멍멍이 프로필
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }: any) => (
+                            <Link
+                              to='/'
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700',
+                              )}
+                            >
+                              로그아웃
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                ) : (
+                  <Link
+                    to='/signin'
+                    className='flex ml-2 items-center rounded-full bg-gray-600 p-1 px-3 text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                  >
+                    로그인
+                  </Link>
+                )}
               </div>
             </div>
           </div>
