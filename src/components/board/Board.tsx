6903e7f6ca90aa28faa2387eb40317c0ace19930
Doyoung.Kim <dyflower@nate.com>
2023-06-17 23:22:30 +0900

    :lipstick: Design : all board design
    전체 게시판의 대략적인 디자인

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import DefaultProfile from '../../assets/icons/DefaultProfile.png';
 import dummy from './dummy.json';
 import SearchForm from '../common/SearchForm';
 
 function Board() {
-  console.log(dummy[0].imageUrl);
   return (
     <>
       <SearchForm />
-      <ul role='list' className='divide-y divide-gray-100 m-5'>
+      <ul role='list' className='divide-y divide-gray-200 m-5'>
         {dummy.map((person) => (
           <li key={person.subject} className='flex justify-between gap-x-6 py-5'>
             <div className='flex gap-x-4'>
@@ -18,16 +16,18 @@ function Board() {
                 alt=''
               />
               <div className='min-w-0 flex-auto'>
-                <p className='text-sm font-bold leading-6 text-gray-900'>{person.name}</p>
+                <div className='flex'>
+                  <p className='text-sm font-bold leading-6 text-gray-900'>{person.name}</p>
+                  <p className='pl-1 text-sm leading-6 text-gray-900'>({person.dog})</p>
+                </div>
                 <p className='mt-1 font-semibold truncate text-md leading-5 text-gray-500'>
                   {person.subject}
                 </p>
               </div>
             </div>
-            <div className='hidden sm:flex sm:flex-col sm:items-end'>
-              <p className='text-sm leading-6 text-gray-900'>{person.dog}</p>
+            <div className='sm:flex sm:flex-col sm:items-end'>
               {
-                <p className='mt-1 text-xs leading-5 text-gray-500'>
+                <p className='mt-7 text-xs leading-5 text-gray-500'>
                   <time>{person.postTime}</time>
                 </p>
               }
